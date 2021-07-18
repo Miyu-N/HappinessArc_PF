fc8b728ab0195a205ca7831c708f5ee040f97980
Miyu-N <myu.star1217@gmail.com>
2021-07-18 12:33:23 +0000

    [Add]contact form

--- app/helpers/public/rooms_helper.rb
+++ app/helpers/public/rooms_helper.rb
@@ -1,29 +1,3 @@
 module Public::RoomsHelper
 
-  # 最新メッセージのデータを取得して表示するメソッド
-  def most_new_message_preview(room)
-    # 最新メッセージのデータを取得する
-    message = room.chats.order(updated_at: :desc).limit(1)
-    # 配列から取り出す
-    message = message[0]
-    # メッセージの有無を判定
-    if message.present?
-      # メッセージがあれば内容を表示
-      tag.p "#{message.text}", class: "dm_list__content__link__box__message"
-    else
-      # メッセージがなければ[ まだメッセージはありません ]を表示
-      tag.p "[ まだメッセージはありません ]", class: "dm_list__content__link__box__message"
-    end
-  end
-
-  # 相手ユーザー名を取得して表示するメソッド
-  def opponent_user(room)
-    # 中間テーブルから相手ユーザーのデータを取得
-    user_rooms = room.user_rooms.where.not(user_id: current_user)
-    # 相手ユーザーの名前を取得
-    name = user_rooms[0].user.name
-    # 名前を表示
-    tag.p "#{name}", class: "dm_list__content__link__box__name"
-  end
-
 end
