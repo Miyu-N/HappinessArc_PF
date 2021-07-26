f58f3690c76004b8d784491a8286583f7d6c9a8e
Miyu-N <myu.star1217@gmail.com>
2021-07-26 05:53:40 +0000

    [Fix]gem --listen

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
   config.action_mailer.delivery_method = :smtp # 簡単なメールの送信の手順
   config.action_mailer.smtp_settings = {
